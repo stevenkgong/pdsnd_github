deb163a12a279ac04a3a204642c9996693c3bd83
Steve Gong <stevenkegong@gmail.com>
2020-08-11 21:07:58 -0700

    updated comments fo the filter function

--- bikeshare_Steve_Gong.py
+++ bikeshare_Steve_Gong.py
@@ -3,6 +3,7 @@ import datetime
 import pandas as pd
 import numpy as np
 
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york': 'new_york_city.csv',
               'washington': 'washington.csv' }
@@ -17,7 +18,7 @@ def get_filters():
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
-        (str) raw_data - Yes or No answer prompt from the user whether they want to see the raw data
+        (str) raw_data - Yes or No input answer prompt from the user whether they want to see the raw data
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
